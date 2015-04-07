2f858cb5297cb278aec2bbb861c19836b779d11f
James Collins <jimbobway200@hotmail.com>
2015-04-07 09:47:52 -0700

    Finished. Added comments and header comment to .java file as requested. Tested with the following criteria: - No Input for Bill amount -> error message is produced prompting user for a valid bill amount - Entered a Bill amount but no radio button selected for tip percentage --> will assume no tip and output $0.00 Tip and bill amount - Entered a bill amount and selected a radio button for tip percentage -> Properly calculates and rounds the tip and final bill amount.

--- app/src/main/java/com/wwu/jimmy/jamescollins_hw1_tipcalculator/MainActivity.java
+++ app/src/main/java/com/wwu/jimmy/jamescollins_hw1_tipcalculator/MainActivity.java
@@ -1,3 +1,22 @@
+// James Collins
+// collinj8@students.wwu.edu
+// CS412 - Mobile Device Programming
+// Week 2 Project
+//
+// Project Description
+// *******************
+// Simple Tip Calculator
+//
+// Instructions
+// *******************
+// -Enter your Bill Amount
+// -Select a Tip Percent Amount that
+// you want to tip your waitor
+// -Then press submit!
+// -You will be presented with the Tip
+// amount as well as the final bill
+// amount to put on the check
+
 package com.wwu.jimmy.jamescollins_hw1_tipcalculator;
 
 import android.support.v7.app.ActionBarActivity;
@@ -6,21 +25,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Formatter;
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -29,6 +36,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Grab fields from user view (activity_main.xml)
         final EditText myEditField = (EditText) findViewById(R.id.billTotal);
         final TextView finalAmount = (TextView) findViewById(R.id.finalDisplay);
         final TextView taxAmount = (TextView) findViewById(R.id.taxDisplay);
@@ -46,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
                     String mealprice = myEditField.getText().toString();
                     float floatingMealPrice = Float.parseFloat(mealprice);
 
-                    //Find out which checkbox is checked
+                    //Find out which checkbox is checked and assign taxPercent
                     RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
                     int id = radioGroup.getCheckedRadioButtonId();
                     double taxPercent;
@@ -62,13 +70,16 @@ public class MainActivity extends ActionBarActivity {
                     {
                         taxPercent = 0.20;
                     }
-                    else
+                    else //User didn't check anything, assume 0 percent tax
                     {
                         taxPercent = 0.0;
                     }
 
+                    //Setup Strings to be put on screen
                     String taxString = "Tax Amount: $" + String.format("%.2f", (taxPercent * floatingMealPrice));
                     String finalBillString = "Full Price: $" + String.format("%.2f", (floatingMealPrice + (taxPercent * floatingMealPrice)));
+
+                    //Push Strings created to user screen (activity_main.xml)
                     finalAmount.setText(finalBillString);
                     taxAmount.setText(taxString);
                 }
@@ -103,5 +114,3 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-
