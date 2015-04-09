cbe5dbaac5b0728a43fab7cf9685fc40fdc3ccc7
James Collins <jimbobway200@hotmail.com>
2015-04-09 07:54:15 -0700

    Added Feature (for fun) - hid the keyboard after the user hits submit

--- app/src/main/java/com/wwu/jimmy/jamescollins_hw1_tipcalculator/MainActivity.java
+++ app/src/main/java/com/wwu/jimmy/jamescollins_hw1_tipcalculator/MainActivity.java
@@ -19,11 +19,13 @@
 
 package com.wwu.jimmy.jamescollins_hw1_tipcalculator;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -33,6 +35,7 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -50,6 +53,12 @@ public class MainActivity extends ActionBarActivity {
             {
                 try
                 {
+                    //Hide the Keyboard after the user hits submit
+                    InputMethodManager inputManager = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+
                     //Get Meal Price From User
                     String mealprice = myEditField.getText().toString();
                     float floatingMealPrice = Float.parseFloat(mealprice);
